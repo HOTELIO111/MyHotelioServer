24106c4ce7bb092265b262bb782e6d0e2fddc368
SharmaG-Dev <99015589+SharmaG-Dev@users.noreply.github.com>
2023-09-13 11:37:38 +0530

    updated Server

---
+++ Model/other/notificationModel.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+
+const schema = new Schema({
+    title: {
+        type: String,
+        default: "Your Notification"
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    recipient: {
+        type: String,
+        required: true,
+    },
+    read: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+
+const NotificationModel = model("notifications", schema)
+
+
+module.exports = NotificationModel; 
