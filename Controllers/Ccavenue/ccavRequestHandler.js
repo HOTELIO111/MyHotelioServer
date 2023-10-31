9dcfb673f45503c06b63dfdab942e6b5d031e748
AbhishekTsFullStack <145094267+AbhishekTsFullStack@users.noreply.github.com>
2023-10-31 17:41:46 +0530

    Updated ccavenue function

--- Controllers/Ccavenue/ccavRequestHandler.js
+++ Controllers/Ccavenue/ccavRequestHandler.js
@@ -1,30 +1,30 @@
-var http = require("http"),
-  fs = require("fs"),
-  ccav = require("./ccavutils"),
-  qs = require("querystring");
+const http = require("http");
+const fs = require("fs");
+const ccav = require("./ccavutils");
+const qs = require("querystring");
 
 module.exports = (request, response) => {
-  var body = "",
-    workingKey = "D946555C26954295EFAB0BF6151B8270", //Put in the 32-Bit key shared by CCAvenues.
-    accessCode = "AVDI01KH74BU39IDUB", //Put in the Access Code shared by CCAvenues.
-    encRequest = "",
-    formbody = "";
+  let body = "";
+  const workingKey = "282763039E883865B27B93BA9C314D5B"; // Put in the 32-Bit key shared by CCAvenues.
+  const accessCode = "AVWB01KH71BU48BWUB"; // Put in the Access Code shared by CCAvenues.
+  let encRequest = "";
+  let formBody = "";
 
   request.on("data", function (data) {
     body += data;
     encRequest = ccav.encrypt(body, workingKey);
-    formbody =
-      '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
-      encRequest +
-      '"><input type="hidden" name="access_code" id="access_code" value="' +
-      accessCode +
-      '"><script language="javascript">document.redirect.submit();</script></form>';
+    formBody = `
+      <form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
+        <input type="hidden" id="encRequest" name="encRequest" value="${encRequest}">
+        <input type="hidden" name="access_code" id="access_code" value="${accessCode}">
+        <script language="javascript">document.redirect.submit();</script>
+      </form>
+    `;
   });
 
   request.on("end", function () {
     response.writeHeader(200, { "Content-Type": "text/html" });
-    response.write(formbody);
+    response.write(formBody);
     response.end();
   });
-  return;
 };
--- Controllers/Ccavenue/ccavResponseHandler.js
+++ Controllers/Ccavenue/ccavResponseHandler.js
@@ -1,33 +1,46 @@
-var http = require("http"),
-  fs = require("fs"),
-  ccav = require("./ccavutils"),
-  qs = require("querystring");
+const http = require("http");
+const fs = require("fs");
+const ccav = require("./ccavutils");
+const qs = require("querystring");
 
 module.exports = (request, response) => {
-  var ccavEncResponse = "",
-    ccavResponse = "",
-    workingKey = "D946555C26954295EFAB0BF6151B8270", //Put in the 32-Bit key shared by CCAvenues.
-    ccavPOST = "";
+  let ccavEncResponse = "";
+  let ccavResponse = "";
+  const workingKey = "282763039E883865B27B93BA9C314D5B"; // Put in the 32-Bit key shared by CCAvenues.
+  let ccavPOST = "";
 
   request.on("data", function (data) {
     ccavEncResponse += data;
     ccavPOST = qs.parse(ccavEncResponse);
-    var encryption = ccavPOST.encResp;
+    const encryption = ccavPOST.encResp;
     ccavResponse = ccav.decrypt(encryption, workingKey);
   });
 
   request.on("end", function () {
-    var pData = "";
-    pData = "<table border=1 cellspacing=2 cellpadding=2><tr><td>";
-    pData = pData + ccavResponse.replace(/=/gi, "</td><td>");
+    let pData = "<table border=1 cellspacing=2 cellpadding=2><tr><td>";
+    pData += ccavResponse.replace(/=/gi, "</td><td>");
     pData = pData.replace(/&/gi, "</td></tr><tr><td>");
-    pData = pData + "</td></tr></table>";
-    htmlcode =
-      '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' +
-      pData +
-      "</center><br></body></html>";
+    pData += "</td></tr></table>";
+
+    const htmlCode = `
+      <html>
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+          <title>Response Handler</title>
+        </head>
+        <body>
+          <center>
+            <font size="4" color="blue"><b>Response Page</b></font>
+            <br>
+            ${pData}
+          </center>
+          <br>
+        </body>
+      </html>
+    `;
+
     response.writeHeader(200, { "Content-Type": "text/html" });
-    response.write(htmlcode);
+    response.write(htmlCode);
     response.end();
   });
 };
