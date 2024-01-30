923bd008721e02304eafa60b84161e6a14e9ef90
AbhishekTsFullStack <145094267+AbhishekTsFullStack@users.noreply.github.com>
2024-01-30 13:45:49 +0530

    Updated new Api

--- Routes/utils.js
+++ Routes/utils.js
@@ -9,6 +9,7 @@ const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { default: axios } = require("axios");
 
 const s3Client = new S3Client({
   region: process.env.REGION,
@@ -17,6 +18,8 @@ const s3Client = new S3Client({
     secretAccessKey: process.env.SECRETACCESSKEY,
   },
 });
+
+const newStorage = multer.memoryStorage();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -37,6 +40,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+const storeMemory = multer({ storage: newStorage });
 
 router.post("/uploadfile/:folder", upload.single("myfile"), (req, res) => {
   const fileName = req.file.filename;
@@ -110,5 +114,47 @@ router.get("/s3/signedurl", async (req, res) => {
       .json({ error: true, message: "Error generating signed URL" });
   }
 });
+
+router.post(
+  "/upload/file/directly",
+  storeMemory.single("file"),
+  async (req, res) => {
+    const { fileName } = req.query;
+    const file = req.file;
+
+    try {
+      const generatePath = `${Date.now()}-${fileName}`;
+
+      const command = new PutObjectCommand({
+        Bucket: "hotelio-images",
+        Key: generatePath,
+        ContentType: file ? file.mimetype : "application/octet-stream",
+      });
+
+      const uploadUrl = await getSignedUrl(s3Client, command, {
+        expiresIn: 120,
+      });
+
+      const _uploadIt = await axios.put(uploadUrl, file.buffer, {
+        headers: {
+          "Content-Type": file.mimetype,
+        },
+      });
+
+      if (_uploadIt.status !== 200)
+        return res
+          .status(400)
+          .json({ error: true, message: "Failed to upload file" });
+
+      res.status(200).json({
+        error: false,
+        message: "Success",
+        fileName: `https://hotelio-images.s3.ap-south-1.amazonaws.com/${generatePath}`,
+      });
+    } catch (error) {
+      res.status(500).json({ error: true, message: error.message });
+    }
+  }
+);
 
 module.exports = router;
