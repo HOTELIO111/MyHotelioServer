8f8c1ee97d2e39983f73b085ffdad5f35a387b3c
Shivam Vishwakarma <shivamvishva552@gmail.com>
2024-11-30 13:10:47 +0530

    bug fixes on routes

--- Controllers/admincontrollers/admincustomerManage.js
+++ Controllers/admincontrollers/admincustomerManage.js
@@ -2,9 +2,19 @@ const CustomerAuthModel = require("../../Model/CustomerModels/customerModel");
 const { DeleteTheCustomer } = require("../../helper/customers/customer_helper");
 
 const GetallCustomer = async (req, res) => {
-  const { userId } = req.query;
+  const { userId, listBooking } = req.query;
   const data = userId ? { _id: userId } : {};
   try {
+    if (listBooking === "true" && userId) {
+      const customer = await CustomerAuthModel.findOne(data).populate(
+        "bookings"
+      );
+      if (!customer)
+        return res.status(404).json({ error: true, message: "no user found" });
+      return res
+        .status(200)
+        .json({ error: false, message: "success", data: customer });
+    }
     const allcustomer = await CustomerAuthModel.find(data).sort({
       createdAt: -1,
     });
